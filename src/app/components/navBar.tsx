098b14f0b58738e2e936c31465b96a958774bc36
pablokampmann <pablo.kampmann@uap.edu.ar>
2023-08-29 18:08:41 -0300

    update

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -1,20 +1,21 @@
+import { BiCategoryAlt } from "react-icons/bi";
+import { SiDigikeyelectronics } from "react-icons/si";
 import { FaShoppingBag } from "react-icons/fa";
 
 export function NavBar() {
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="/" className="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kampmann Store</span>
-                </a>
-                <div className="flex md:order-2">
-                    <button>
-                        <FaShoppingBag size={30} />
-                    </button>
-                </div>
+            <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+                <button className="flex items-center">
+                    <BiCategoryAlt size={30} className="text-xl mr-2" />
+                </button>
+                <button className="flex items-center">
+                    <SiDigikeyelectronics size={40} className="text-xl mx-4" />
+                </button>
+                <button className="flex items-center">
+                    <FaShoppingBag size={30} className="text-xl ml-2" />
+                </button>
             </div>
         </nav>
     );
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,179 @@
-import Image from 'next/image'
+import React from 'react';
 
 export default function Home() {
   return (
-    <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto mb-5'>
-      <div className="2xl:mx-auto 2xl:container py-4 px-4 sm:px-6 xl:px-20 2xl:px-0 w-full">
-        <div className="flex flex-col jusitfy-center items-center space-y-10">
-          <div className="flex flex-col justify-center items-center ">
-            <h1 className="mb-2 mt-0 text-3xl md:text-5xl font-medium leading-tight text-white text-center py-4 md:border-b-2 md:border-white relative">
-              Selecciona una Categoría
-              <span className="block md:hidden absolute bottom-[-40px] left-1/2 transform -translate-x-1/2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white" className="w-12 h-12">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-                </svg>
-              </span>
-            </h1>
+    <div className="bg-white">
+      <h1 className="mt-4 text-3xl md:text-4xl lg:text-5xl font-bold text-center text-gray-800 p-4">
+        Confiabilidad y seguridad en cada compra ✓
+      </h1>
+      <hr className="border-t-2 border-gray-800 w-3/5 mx-auto my-4" />
 
-          </div>
-          <div className="flex justify-center">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 md:gap-x-8 w-full">
-              <div className="flex flex-col space-y-4 md:space-y-8 mt-4 md:mt-0">
-                <div className="relative group flex justify-center items-center h-full w-full">
-                  <img className="object-center object-cover h-full w-full" src="https://i.ibb.co/SXZvYHs/irene-kredenets-DDqx-X0-7v-KE-unsplash-1.png" alt="shoe-image" />
-                  <button className="dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">Shoes</button>
-                  <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
-                </div>
+      <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Promos!</h2>
 
-                <div className="relative group flex justify-center items-center h-full w-full">
-                  <img className="object-center object-cover h-full w-full" src="https://i.ibb.co/Hd1pVxW/louis-mornaud-Ju-6-TPKXd-Bs-unsplash-1-2.png" alt="watch-image" />
-                  <button className="dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">Watches</button>
-                  <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
-                </div>
-              </div>
+        <div className="mt-6 overflow-x-auto flex">
 
-              <div className="relative group justify-center items-center h-full w-full col-span-2 lg:col-span-1">
-                <img className="object-center object-cover h-full w-full" src="https://i.ibb.co/PTtRBLL/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png" alt="girl-image" />
-                <button className="dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">Accessories</button>
-                <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
-              </div>
+
+        <div className="group relative border border-gray-800 p-4 rounded-md mr-4">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img src="/auri_prueba.jpg" alt="auri" className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
             </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Auriculares Bluetooth In-Ear F9-5
+                  </a>
+                </h3>
+              </div>
+              <p className="text-sm font-bold text-gray-900">$35</p>
+            </div>
+            <hr className="mt-4 border-t-2 border-gray-800 w-full" />
           </div>
 
+
+          <div className="group relative border border-gray-800 p-4 rounded-md mr-4">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img src="/auri_prueba.jpg" alt="auri" className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Auriculares Bluetooth In-Ear F9-5
+                  </a>
+                </h3>
+              </div>
+              <p className="text-sm font-bold text-gray-900">$35</p>
+            </div>
+            <hr className="mt-4 border-t-2 border-gray-800 w-full" />
+          </div>
+
+          <div className="group relative border border-gray-800 p-4 rounded-md mr-4">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img src="/auri_prueba.jpg" alt="auri" className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Auriculares Bluetooth In-Ear F9-5
+                  </a>
+                </h3>
+              </div>
+              <p className="text-sm font-bold text-gray-900">$35</p>
+            </div>
+            <hr className="mt-4 border-t-2 border-gray-800 w-full" />
+          </div>
+
+          <div className="group relative border border-gray-800 p-4 rounded-md mr-4">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img src="/auri_prueba.jpg" alt="auri" className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Auriculares Bluetooth In-Ear F9-5
+                  </a>
+                </h3>
+              </div>
+              <p className="text-sm font-bold text-gray-900">$35</p>
+            </div>
+            <hr className="mt-4 border-t-2 border-gray-800 w-full" />
+          </div>
 
         </div>
-
       </div>
+
+
+      <div className="mx-auto max-w-2xl px-4 mb-8 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Productos</h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+
+          <div className="group relative border border-gray-800 p-4 rounded-md">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img src="/auri_prueba.jpg" alt="auri" className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Auriculares Bluetooth In-Ear F9-5
+                  </a>
+                </h3>
+              </div>
+              <p className="text-sm font-bold text-gray-900">$35</p>
+            </div>
+            <hr className="mt-4 border-t-2 border-gray-800 w-full" />
+          </div>
+
+
+          <div className="group relative border border-gray-800 p-4 rounded-md">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img src="/auri_prueba.jpg" alt="auri" className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Auriculares Bluetooth In-Ear F9-5
+                  </a>
+                </h3>
+              </div>
+              <p className="text-sm font-bold text-gray-900">$35</p>
+            </div>
+            <hr className="mt-4 border-t-2 border-gray-800 w-full" />
+          </div>
+
+          <div className="group relative border border-gray-800 p-4 rounded-md">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img src="/auri_prueba.jpg" alt="auri" className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Auriculares Bluetooth In-Ear F9-5
+                  </a>
+                </h3>
+              </div>
+              <p className="text-sm font-bold text-gray-900">$35</p>
+            </div>
+            <hr className="mt-4 border-t-2 border-gray-800 w-full" />
+          </div>
+
+          <div className="group relative border border-gray-800 p-4 rounded-md">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img src="/auri_prueba.jpg" alt="auri" className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Auriculares Bluetooth In-Ear F9-5
+                  </a>
+                </h3>
+              </div>
+              <p className="text-sm font-bold text-gray-900">$35</p>
+            </div>
+            <hr className="mt-4 border-t-2 border-gray-800 w-full" />
+          </div>
+
+        </div>
+      </div>
+
+
     </div>
 
-  )
+  );
 }
